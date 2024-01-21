0dfe1f15700836d28f807eb0795a82ab8d598c48
XiuyuLiang <Liang.Xiuyu@access-company.com>
2024-01-21 21:37:12 +0900

    Implement customers and orders logic  (#1)
    * create model customer
    
    * create model order
    
    * migrate db
    
    * implement customer info api and page
    
    * implement orders api and page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'customers/:id/rank_info', to: 'customers#rank_info'
+  get 'orders/customer_orders/:id', to: 'orders#customer_orders'
 end
