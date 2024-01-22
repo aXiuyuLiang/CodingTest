047233fa8675437ec23a90fc399e927eb91fe0b0
XiuyuLiang <Liang.Xiuyu@access-company.com>
2024-01-22 09:01:27 +0900

    pass customer_id to frontend

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,12 +4,12 @@ class OrdersController < ApplicationController
     MIN_GOLD_AMOUNT = 500
 
     def customer_orders
-        customer_id = params[:id]
+        @customer_id = params[:id]
         page_num = params[:page].present? ? params[:page].to_i : 1
         start_of_last_year = Date.new(Date.today.year - 1, 1, 1)
         start_index = (page_num - 1) * PAGE_SIZE
         end_index = start_index + PAGE_SIZE - 1
-        @orders = Order.where('external_customer_id = ? AND ordered_at >= ?', customer_id, start_of_last_year)[start_index..end_index]
+        @orders = Order.where('external_customer_id = ? AND ordered_at >= ?', @customer_id, start_of_last_year)[start_index..end_index]
         @page_num = page_num
         @page_size = PAGE_SIZE
 
